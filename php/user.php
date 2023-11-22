9492419f2ec74fd62f223b8b63510486557aeaff
CarlosMar17 <148661375+CarlosMar17@users.noreply.github.com>
2023-11-22 02:27:41 -0600

    Se creo la base para las paginas html, se mejoro la funcionabilidad para el inicio y cierre de sesion

--- php/user.php
+++ php/user.php
@@ -1,36 +1,23 @@
 <?php
-// Conectar a la base de datos
 include("conexion.php");
 
-// Obtener los datos del formulario de inicio de sesión
 $username = $_POST["usuario"];
 $password = $_POST["password"];
-
-// Verificar si el usuario existe
+// Validacion datos con base de datos
 $sql = "SELECT * FROM usuario WHERE NombreU='$username'";
 $result = $conn->query($sql);
-
-// Verificar si hay errores en la consulta SQL
 if (!$result) {
     echo "Error en la consulta: " . $conn->error;
     return;
 }
-
-// Verificar si se encontró un usuario
 if ($result->num_rows === 0) {
     echo "Usuario no encontrado";
     return;
 }
-
-// Obtener la fila de resultados
 $row = $result->fetch_assoc();
-
-// Validar que la contraseña sea correcta
 if ($password !== $row['Contraseña']) {
     echo "Contraseña incorrecta";
     return;
 }
-
-// Inicio de sesión exitoso
 echo "Exito";
 ?>
--- scripts.js
+++ scripts.js
@@ -1,87 +1,114 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-    $(document).ready(function(){
-        $("#registerMbutton").on("click", function(){
+    $(document).ready(function() {
+        $("#registerMbutton").on("click", function() {
             $("#loginModal").modal("hide");
-            $('body').removeClass('modal-open');
-            $('.modal-backdrop').remove();
             $("#registerModal").modal("show");
         });
-
-    //registro
-    $("#registerButton").on("click", function () {
-        var email = $("#emailRegistro").val();
-        var usuario = $("#usuarioRegistro").val();
-        var password = $("#passwordRegistro").val();
-        if (usuario === "" || password === ""|| email === "") {
-            alert("Por favor, completa todos los campos.");
-            return;
-          }
-        $.ajax({
-            type: "POST",
-            url: "php/register.php",
-            data: { email: email, usuario: usuario, password: password },
-            success: function (response) {
-                alert(response);
-                if (response === "Registro exitoso") {
-                }
+    
+        //registro
+        $("#registerButton").on("click", function () {
+            var email = $("#emailRegistro").val();
+            var usuario = $("#usuarioRegistro").val();
+            var password = $("#passwordRegistro").val();
+            if (usuario === "" || password === "" || email === "") {
+                alert("Por favor, completa todos los campos.");
+                return;
             }
+            $.ajax({
+                type: "POST",
+                url: "php/register.php",
+                data: { email: email, usuario: usuario, password: password },
+                success: function (response) {
+                    alert(response);
+                    if (response === "Registro exitoso") {
+                    }
+                }
+            });
         });
-    });
 
-    // Validación de inicio de sesión
+
+    //inicio de sesión
     $("#loginMbutton").on("click", function (event) {
         event.preventDefault();
-    
-        // Validar que el usuario y la contraseña no estén vacíos
         var usuario = $("#username").val();
         var password = $("#password").val();
         if (usuario === "" || password === "") {
-        alert("Por favor, completa todos los campos.");
-        return;
+            alert("Por favor, completa todos los campos.");
+            return;
         }
-    
-        // Verificar existencia en la base de datos
         $.ajax({
             type: "POST",
             url: "php/user.php",
             data: { usuario: usuario, password: password },
             success: function(response) {
-                console.log(response); // Muestra la respuesta en la consola para depurar
+                console.log(response);
                 if (response === "Usuario no encontrado") {
                     alert("Nombre de usuario no encontrado.");
                 } else if (response === "Contraseña incorrecta") {
                     alert("Contraseña incorrecta.");
                 } else if (response === "Exito") {
+                    // Cambio boton
+                    localStorage.setItem("usuario", usuario);
                     $("#loginModal").modal("hide");
                     $('body').removeClass('modal-open');
                     $('.modal-backdrop').remove();
-                    // alert("Inicio de sesión exitoso.");
-                    // Crear el código del nuevo botón de dropdown
                     var dropdownButton = `
                         <div class="dropdown">
-                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <button class="btn btn-secondary dropdown-toggle mr-2" type="button" id="DDusuario" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 ${usuario}
                             </button>
                             <div class="dropdown-menu">
-                                <a class="dropdown-item" href="#">Opción 1</a>
-                                <a class="dropdown-item" href="#">Opción 2</a>
-                                <a class="dropdown-item" href="#">Opción 3</a>
+                            <a class="dropdown-item" href="Administracion.html">Administracion</a>
+                            <a class="dropdown-item" href="GestionC.html">Gestor de citas</a>
+                            <a class="dropdown-item" id="logoutLink" href="#">Cerrar sesión</a>
                             </div>
                         </div>
                     `;
-
-                    // Reemplazar el botón de inicio de sesión por el dropdown
                     $("#ISbotton").replaceWith(dropdownButton);
-                
-            
+                    $("#logoutLink").on("click", function() {
+                        var loginButton = `
+                            <button type="button" id="ISbotton" class="btn btn-primary mr-2" data-toggle="modal" data-target="#loginModal">
+                                Iniciar sesión
+                            </button>
+                        `;
+                        $("#DDusuario").replaceWith(loginButton);
+                        location.reload();
+                    });
                 }
             },
-            error: function(jqXHR, textStatus, errorThrown) {
+            error: function (jqXHR, textStatus, errorThrown) {
                 console.error("Error en la solicitud AJAX: " + textStatus, errorThrown);
             }
         });
     });
-    });
+
+    //Cambio boton usuario
+    var storedUsuario = localStorage.getItem("usuario");
+    if (storedUsuario) {
+        var dropdownButton = `
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle mr-2" type="button" id="DDusuario" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    ${storedUsuario}
+                </button>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="Administracion.html">Administracion</a>
+                    <a class="dropdown-item" href="GestionC.html">Gestor de citas</a>
+                    <a class="dropdown-item" id="logoutLink" href="#">Cerrar sesión</a>
+                </div>
+            </div>
+        `;
+        $("#ISbotton").replaceWith(dropdownButton);
+        $("#logoutLink").on("click", function() {
+            var loginButton = `
+                <button type="button" id="ISbotton" class="btn btn-primary mr-2" data-toggle="modal" data-target="#loginModal">
+                    Iniciar sesión
+                </button>
+            `;
+            $("#DDusuario").replaceWith(loginButton);
+            localStorage.removeItem("usuario");
+            location.reload();
+        });
+    }
+})    
 });
-        
